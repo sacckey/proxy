c727ee8a70bf576d3cb3d8be9625b67147934be6
sacckey <sackeyad24@gmail.com>
2024-07-15 20:23:13 +0900

    Add rubyboy.wasm

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,38 @@
 import { Hono } from 'hono'
+import { cache } from 'hono/cache'
+import { cors } from 'hono/cors'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(
+  '/rubyboy.wasm',
+  cors({
+    origin: '*',
+    allowHeaders: [],
+    allowMethods: ['GET'],
+    exposeHeaders: [],
+    maxAge: 600,
+    credentials: false,
+  })
+)
+
+app.get('/rubyboy.wasm',
+  cache({
+    cacheName: 'rubyboy',
+    cacheControl: 'public, max-age=14400'
+  }),
+  async (_c) => {
+    const response = await fetch('https://github.com/sacckey/rubyboy/releases/latest/download/rubyboy.wasm')
+
+    // Clone the response to return a response with modifiable headers
+    const newResponse = new Response(response.body, {
+      headers: {
+        'Content-Type': 'application/wasm',
+      }
+    })
+
+    return newResponse
+  }
+)
 
 export default app
